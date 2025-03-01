29be08b7c7e811a9be9c06dd1c5cf0eaa42e9540
Joshi Sourabha <joshisourabha@Joshis-MacBook-Pro.local>
2025-03-01 09:47:33 +0530

    Intruduction of JS

---
+++ 01_Intruduction/02_Variables.js
@@ -0,0 +1,19 @@
+// Good way of writting variable names in JS
+
+// Declare variable with diffrent data types : Number, String, Boolean, Undefined and Null
+
+let firstName = "Joshi";
+let lastName = "Sourabha";
+let currentCity = "Bangalore";
+let currentState = "Karnataka";
+let age = 25;
+let isMarried = false;
+let isPassive; // undefined
+let planet = null; // No value
+
+// Declare variable with numbers using const 
+
+const PI = 3.142
+const GRAVITY = 9.81
+
+
---
+++ 01_Intruduction/03_MultiLinevariables.js
@@ -0,0 +1,10 @@
+// declare variables in single line
+
+let firstName = "Sourabha", lastName = "Joshi", age = 25;
+console.log(firstName, " ", lastName, " ", age);
+
+// Variable can be declare with multiline using / symbol
+
+let fullName = firstName  + " " + 
+                lastName;
+console.log(fullName);
